f07f0e4d222c68c840d1c6ecaf578960c13bf422
Bihami Bence <126898414+BihaBen@users.noreply.github.com>
2023-05-30 11:05:33 +0200

    Update strokePred.py

--- strokePred.py
+++ strokePred.py
@@ -25,82 +25,6 @@ file = pd.read_csv('healthcare-dataset-stroke-data.csv')
 df = file
 
 #------------------------------------------------------------------
-#TESZT
-#------------------------------------------------------------------
-# Beolvasás tesztelése
-print("\nAz adatok sorinak száma:" + " " + str(len(df.index)))
-print("Legnagyobb életkor")
-print((df["age"].max()))
-print("Legkissebb életkor (létezik pár hetes baba)")
-print((df["age"].min()))
-print("Átlagos életkor")
-print((df["age"].mean()))
-
-print("\nLegnagyobb BMI")
-print((df["bmi"].max()))
-print("Legkissebb BMI")
-print((df["bmi"].min()))
-print("Átlagos BMI")
-print((df["bmi"].mean()))
-
-print("\nLegnagyobb cukorszint")
-print((df["avg_glucose_level"].max()))
-print("Legkissebb cukorszint")
-print((df["avg_glucose_level"].min()))
-print("Átlagos cukorszint")
-print((df["avg_glucose_level"].mean()))
-
-#------------------------------------------------------------------
-# TISZTÍTÁS
-#------------------------------------------------------------------
-# Minden életkor legyen float
-df['age'] = df[["age"]].fillna(0).astype(float) # Létezik integer és float típusú 
-
-# NULL tipusok cserélése Numpy tipusu NULL-ra
-df.replace('Nan',np.nan,inplace=True)
-df.replace('nan',np.nan,inplace=True)
-df.replace('N/A',np.nan,inplace=True)
-
-# Mennyi NULL van a DataSet-ben
-print(df.isna().sum())
-
-print("\nAz adatok sorinak száma:" + " " + str(len(df.index)))
-
-# Férfi = 1 Nő = 0
-df["gender"] = df["gender"].replace({"Female": 0 ,"Male": 1, "Other": 2})
-
-# Valaha megházasodott-> igen = 1 nem = 0
-df["ever_married"] = df["ever_married"].replace({"Yes": 1, "No": 0})
-
-# Városban vagy vidéken él-> városban = 1 vidéken = 0
-df["Residence_type"] = df["Residence_type"].replace({"Urban": 1, "Rural": 0})
-
-# Dohányzási szokások számmá alakítás -> "soha": 0, "néha": 1, "dohányzik": 2, "ismeretlen": 3
-df["smoking_status"] = df["smoking_status"].replace({"never smoked": 0, "formerly smoked": 1, "smokes": 2, "Unknown": 3 })
-
-# Munkavégzés tipusa dinamikusan egy dictionary-be teszi a kulcs-érték párokat
-workType_list = df["work_type"].tolist()
-workType_map = {work_type: i for i, work_type in enumerate(df["work_type"].unique())}
-df["work_type"] = df["work_type"].replace(workType_map)
-workNumb_list = df["work_type"].tolist()
-my_dict = dict(zip(workType_list, workNumb_list))
-my_dict = {k: v for k, v in set(my_dict.items())}
-
-old_rowN=len(df.index)
-#avg_glucose_level -> Magyar értékké konvertálás
-df.avg_glucose_level = [round(x / 18.016,2) for x in df.avg_glucose_level]
-
-#bmi -> Amerikai és Magyar számítási különbségek vannak, de az eredmény megegyezik [7 és 40 közötti BMI]
-df = df[(df['bmi']> 7) & (df['bmi'] < 40)]
-
-#smoking -> Számomra hihetetlen, ha 10-12 évesen cigizik az illető és még nyilatkozik is róla
-df = df.loc[~((file['age'] < 10) & (df['smoking_status'] == 'smokes')) | ((df['age'] < 10) & (df['smoking_status'] == 'formerly smoked'))]
-
-# Adatok sorainak csökkenése
-print("\nTörölt adatok száma:" + " " + str(old_rowN - len(df.index)))
-print("Maradt még:" + " " + str(len(df.index)))
-
-#------------------------------------------------------------------
 # TANÍTÁS
 #------------------------------------------------------------------
 # Feature-ökre és labelekre felbontás
@@ -112,10 +36,10 @@ Y= df['stroke'].values
 X_train, x_test, Y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=73)
 
 # Teszt
-print("A tanító adatok száma: " + str(X_train.shape))
-print("A tanító adatok célváltozóinak száma: " + str(Y_train.shape))
-print("\nA predikáló adatok száma: " + str(x_test.shape))
-print("A predikáló célváltozóinak száma: " + str(y_test.shape))
+#print("A tanító adatok száma: " + str(X_train.shape))
+#print("A tanító adatok célváltozóinak száma: " + str(Y_train.shape))
+#print("\nA predikáló adatok száma: " + str(x_test.shape))
+#print("A predikáló célváltozóinak száma: " + str(y_test.shape))
 
 #------------------------------------------------------------------
 # DÖNTÉSI FA
