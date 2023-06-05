7f363da288ae89f28b7bf4737c15b074de75f6fb
Bihami Bence <126898414+BihaBen@users.noreply.github.com>
2023-06-05 13:13:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,14 @@
 import streamlit as st
 
-from strokePred import rf,knn,dtc, svm
-from strokePred import x_test, y_test
-
+from strokePred import dtc, rf, knn, svm
+from strokePred import X_test, y_test
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, auc
+#Metrikák
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
 
 def add_bg_from_url():
     st.markdown(
@@ -28,7 +31,7 @@ def main():
 
     if button_pressed1:
         # Tesztadatok előrejelzése
-        y_pred = rf.predict_proba(x_test)[:, 1]  # Első oszlopban a pozitív osztály előrejelzéseinek valószínűségeit tároljuk
+        y_pred = rf.predict(X_test)  # Első oszlopban a pozitív osztály előrejelzéseinek valószínűségeit tároljuk
 
         # ROC görbe számítása
         fpr, tpr, thresholds = roc_curve(y_test, y_pred)
@@ -49,13 +52,46 @@ def main():
         st.pyplot(plt)
     if button_pressed2:
         with st.sidebar:
-            rf_accuracy = rf.score(x_test, y_test)
-            knn_accuracy = knn.score(x_test, y_test)
-            svm_accuracy = svm.score(x_test, y_test)
+            
+            y_pred_dtc = dtc.predict(X_test)
+            y_pred_rf = dtc.predict(X_test)
+            y_pred_knn = dtc.predict(X_test)
+            y_pred_svm = dtc.predict(X_test)
+            
+            accuracy_dtc = rf.accuracy_score(X_test, y_pred_dtc)
+            accuracy_rf = rf.accuracy_score(X_test, y_pred_rf)
+            accuracy_knn = knn.accuracy_score(X_test, y_pred_knn)
+            accuracy_svm = svm.accuracy_score(X_test, y_pred_svm)
+            
+            Recall_dtc = rf.recall_score(X_test, y_pred_dtc)
+            Recall_rf = rf.recall_score(X_test, y_pred_rf)
+            Recall_knn = knn.recall_score(X_test, y_pred_knn)
+            Recall_svm = svm.recall_score(X_test, y_pred_svm)
+            
+            f1_dtc = rf.f1_score(X_test, y_pred_dtc)
+            f1_rf = rf.f1_score(X_test, y_pred_rf)
+            f1_knn = knn.f1_score(X_test, y_pred_knn)
+            f1_svm = svm.f1_score(X_test, y_pred_svm)
+           
             # Kiíratás
-            st.write('RandomForest pontossága: {}%'.format(round((rf_accuracy*100),2)))
-            st.write('KNN pontossága: {}%'.format(round((knn_accuracy*100),2)))
-            st.write('SVM pontossága: {}%'.format(round((svm_accuracy*100),2)))
+            st.write('ACCURACY <br>')
+            st.write('RandomForest pontossága: {}%'.format(round((accuracy_dtc*100),2)))
+            st.write('RandomForest pontossága: {}%'.format(round((accuracy_rf*100),2)))
+            st.write('KNN pontossága: {}%'.format(round((accuracy_knn*100),2)))
+            st.write('SVM pontossága: {}%'.format(round((accuracy_svm*100),2)))
+            
+            
+            st.write('<br>RECALL')
+            st.write('RandomForest pontossága: {}%'.format(round((Recall_dtc*100),2)))
+            st.write('RandomForest pontossága: {}%'.format(round((Recall_rf*100),2)))
+            st.write('KNN pontossága: {}%'.format(round((Recall_knn*100),2)))
+            st.write('SVM pontossága: {}%'.format(round((Recall_svm*100),2)))
+            
+            st.write('<br>F1')
+            st.write('RandomForest pontossága: {}%'.format(round((f1_dtc*100),2)))
+            st.write('RandomForest pontossága: {}%'.format(round((f1_rf*100),2)))
+            st.write('KNN pontossága: {}%'.format(round((f1_knn*100),2)))
+            st.write('SVM pontossága: {}%'.format(round((f1_svm*100),2)))
        
 
 if __name__ == '__main__':
