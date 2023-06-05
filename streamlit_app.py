39cc07478e5ebfe91d5d786cf6df8aa5775d8e59
Bihami Bence <126898414+BihaBen@users.noreply.github.com>
2023-06-05 14:09:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,8 +41,44 @@ def main():
         
     if button_pressed2:
         with st.sidebar:
-           y_pred_rf = dtc.predict(X_test)
-           st.write(rf.accuracy_score(X_test, y_pred_dtc))
+            y_pred_dtc = dtc1.predict(X_test)
+            y_pred_rf = rf1.predict(X_test)
+            y_pred_knn = knn1.predict(X_test)
+            y_pred_svm = svm1.predict(X_test)
+            
+            accuracy_dtc = dtc1.accuracy_score(X_test, y_pred_dtc)
+            accuracy_rf = rf1.accuracy_score(X_test, y_pred_rf)
+            accuracy_knn = knn1.accuracy_score(X_test, y_pred_knn)
+            accuracy_svm = svm1.accuracy_score(X_test, y_pred_svm)
+            
+            Recall_dtc = dtc1.recall_score(X_test, y_pred_dtc)
+            Recall_rf = rf1.recall_score(X_test, y_pred_rf)
+            Recall_knn = knn1.recall_score(X_test, y_pred_knn)
+            Recall_svm = svm1.recall_score(X_test, y_pred_svm)
+            
+            f1_dtc = dtc1.f1_score(X_test, y_pred_dtc)
+            f1_rf = rf1.f1_score(X_test, y_pred_rf)
+            f1_knn = knn1.f1_score(X_test, y_pred_knn)
+            f1_svm = svm1.f1_score(X_test, y_pred_svm)
+           
+            # Kiíratás
+            st.write('ACCURACY')
+            st.write('RandomForest pontossága: {}%'.format(round((accuracy_dtc*100),2)))
+            st.write('RandomForest pontossága: {}%'.format(round((accuracy_rf*100),2)))
+            st.write('KNN pontossága: {}%'.format(round((accuracy_knn*100),2)))
+            st.write('SVM pontossága: {}%'.format(round((accuracy_svm*100),2)))
+            
+            st.write('<br>RECALL')
+            st.write('RandomForest pontossága: {}%'.format(round((Recall_dtc*100),2)))
+            st.write('RandomForest pontossága: {}%'.format(round((Recall_rf*100),2)))
+            st.write('KNN pontossága: {}%'.format(round((Recall_knn*100),2)))
+            st.write('SVM pontossága: {}%'.format(round((Recall_svm*100),2)))
+            
+            st.write('<br>F1')
+            st.write('RandomForest pontossága: {}%'.format(round((f1_dtc*100),2)))
+            st.write('RandomForest pontossága: {}%'.format(round((f1_rf*100),2)))
+            st.write('KNN pontossága: {}%'.format(round((f1_knn*100),2)))
+            st.write('SVM pontossága: {}%'.format(round((f1_svm*100),2)))
             
        
 
